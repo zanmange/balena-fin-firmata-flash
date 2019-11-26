d46a2f28ae805577a4a6da999d4fbff540eaf279
Alex <alex.bucknall@gmail.com>
2019-11-26 23:24:31 +0000

    Modified start.sh to always grab latest hex

--- co-processor/app/start.sh
+++ co-processor/app/start.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+# Get latest Firmata release
+curl -s https://api.github.com/repos/balena-io/balena-fin-coprocessor-firmata/releases/latest \
+| grep "browser_download_url.*hex" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+mv *.hex firmware/firmata-balena-latest.hex
+
+# Start server
 node index.js
